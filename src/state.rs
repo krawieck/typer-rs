224345fd6d6e71cc35e7fd51d88d869b7f4b3586
krawieck <fil.krawczyk@gmail.com>
2019-05-25 22:58:49 +0200

    forgot to add pub to struct fields

--- src/state.rs
+++ src/state.rs
@@ -1,25 +1,25 @@
 /// struct representing current state of the program
 pub struct State {
     /// vector containing all the text
-    text: Vec<String>,
-    current_text_index: usize,
-    current_word_index: usize,
-    stats: Vec<Stat>,
-    current_errors: Vec<char>,
+    pub text: Vec<String>,
+    pub current_text_index: usize,
+    pub current_word_index: usize,
+    pub stats: Vec<Stat>,
+    pub current_errors: Vec<char>,
 }
 
 /// struct representing statistics for current word
 pub struct Stat {
     /// time it took to complete this word
-    time: f64,
+    pub time: f64,
     /// how many mistakes were made during writing of this word
-    mistakes: usize,
+    pub mistakes: usize,
 
-    word_len: usize,
+    pub word_len: usize,
 }
 
 impl State {
-    fn from(text: Vec<String>) -> State {
+    pub fn from(text: Vec<String>) -> State {
         return State {
             text: text,
             current_text_index: 0,
