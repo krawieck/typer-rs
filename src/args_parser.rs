2db952f2c70f0b70d9a2bfec22ab1e6bf8e4d08a
krawieck <fil.krawczyk@gmail.com>
2019-05-25 21:43:16 +0200

    args parser added

--- src/args_parser.rs
+++ src/args_parser.rs
@@ -3,7 +3,7 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "ez-renamer")]
 pub struct Args {
-	/// take text from stdin
-	#[structopt(name = "-")]
-	pub take_from_stdin: bool,
+    /// take text from stdin
+    #[structopt(name = "-")]
+    pub take_from_stdin: bool,
 }
