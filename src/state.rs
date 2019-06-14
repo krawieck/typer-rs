86b1a7363b5a2026877ee0eb93e860559bda43d5
krawieck <fil.krawczyk@gmail.com>
2019-06-14 12:29:01 +0200

    added stats measurement

--- src/state.rs
+++ src/state.rs
@@ -8,13 +8,15 @@ pub struct State {
     pub stats: Vec<Stat>,
     pub current_errors: Vec<char>,
     pub finished: bool,
+    // timestamp used for calculating how long it took to write one word
+    pub timestamp: std::time::SystemTime,
 }
 
 /// struct representing statistics for current word
 #[derive(Debug)]
 pub struct Stat {
     /// time it took to complete this word
-    pub time: f64,
+    pub time: std::time::Duration,
     /// how many mistakes were made during writing of this word
     pub mistakes: usize,
 
@@ -30,15 +32,28 @@ impl State {
             stats: vec![],
             current_errors: vec![],
             finished: false,
+            timestamp: std::time::SystemTime::now(),
         };
     }
 
     pub fn update(&mut self, input: crossterm::KeyEvent) {
-        use crossterm::{KeyEvent};
+        use crossterm::KeyEvent;
+        use std::time::SystemTime;
 
         if self.finished {
             return;
         }
+
+        // create instance of Stat object for current word, if not present
+        if self.current_text_index == self.stats.len() {
+            self.stats.push(Stat {
+                time: std::time::Duration::new(0, 0),
+                mistakes: 0,
+                word_len: self.text[self.current_text_index].len(),
+            });
+            self.timestamp = SystemTime::now()
+        }
+
         match input {
             KeyEvent::Backspace => {
                 if self.current_errors.is_empty() {
@@ -57,13 +72,18 @@ impl State {
                 if !self.current_errors.is_empty() {
                     // if there are any errors already stacked up, add this one too and return
                     self.current_errors.push(key);
+                    self.stats[self.current_text_index].mistakes += 1
                 } else if self.current_word_index == self.text[self.current_text_index].len() {
                     // space after word
                     if key == ' ' {
+                        // finished word
+                        self.stats[self.current_text_index].time =
+                            self.timestamp.elapsed().expect("couldn't calculate time");
                         self.current_word_index = 0;
                         self.current_text_index += 1;
                     } else {
                         self.current_errors.push(key);
+                        self.stats[self.current_text_index].mistakes += 1
                     }
                 } else {
                     let curr_index = self.text[self.current_text_index]
@@ -76,10 +96,14 @@ impl State {
                         self.current_word_index += 1;
                     } else {
                         self.current_errors.push(key);
+                        self.stats[self.current_text_index].mistakes += 1
                     }
                     if self.current_text_index == self.text.len() - 1
                         && self.current_word_index == self.text.last().unwrap().len()
                     {
+                        // finished everything
+                        self.stats[self.current_text_index].time =
+                            self.timestamp.elapsed().expect("couldn't calculate time");
                         self.finished = true;
                     }
                 }
