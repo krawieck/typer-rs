4dde05620a008d8cd8d75836b0d8c7801dd892d4
krawieck <fil.krawczyk@gmail.com>
2019-12-15 19:22:28 +0100

    Add `input` to `Args`

--- src/args_parser.rs
+++ src/args_parser.rs
@@ -6,4 +6,12 @@ pub struct Args {
     /// take text from stdin
     #[structopt(name = "-")]
     pub take_from_stdin: bool,
+
+    /// text that should be then written by the user, or path to a file, or link
+    /// to a website, the text should be downloaded from
+    ///
+    /// example:
+    /// typer-rs -i "$(fortune)"
+    #[structopt(short, long, default_value = "")]
+    pub input: String,
 }
